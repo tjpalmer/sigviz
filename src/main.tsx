0f2f794b62faca9c2f964c18d9f77764059ae49c
Tom <tjpalmer@tjpalmer.com>
2017-07-15 10:38:03 -0700

    Trying react

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,21 @@
 // import {createElement, Component, render} from 'rax';
 // import {Text} from 'rax-text';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
 
-let React = {
-  createElement(type: Function, props: any, content: any) {
-    console.log('createElement', type, props, content);
-    return [type, props, content];
-  }
-};
+// let React = {
+//   createElement(type: Function | string, props: any, content: any) {
+//     console.log('createElement', type, props, content);
+//     return [type, props, content];
+//   }
+// };
 
-declare namespace JSX {
-  type Element = any;
-}
+// declare namespace JSX {
+//   type Element = any;
+//   interface IntrinsicElements {
+//     div: any;
+//   }
+// }
 
 function yo(strings: TemplateStringsArray, ...keys: any[]) {
   console.log(strings, keys);  
@@ -21,11 +26,16 @@ function yo(strings: TemplateStringsArray, ...keys: any[]) {
 function init() {
   console.log('Hi!');
   let name = 'world';
-  console.log(<Hello name={name}><Hello>Inside!</Hello></Hello>);
+  // console.log(<Hello name={name}><Hello>Inside!</Hello></Hello>);
+  // console.log(<div>Hi</div>);
   console.log(yo`<Hello name=${name}><Hello>\more</Hello></Hello>`);
+  let hi = new Hello();
+  hi.hi();
+  ReactDOM.render(<h1>Hello, world!</h1>, document.getElementById('root'));
 }
 
 class Hello {
+  hi = () => console.log('Hi from', this);
   render() {
     console.log('render!');
   }
